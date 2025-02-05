55dd575efc0c5372c90e887df3f0aabe68774bc6
Lekan Sofuyi <adtech9000@gmail.com>
2025-02-05 01:29:17 +0100

    Commentd out scriptEL function

--- js/script.js
+++ js/script.js
@@ -170,18 +170,18 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-(function() {
-    const scriptEl = document.currentScript;
-    if (!scriptEl) return;
-    const insEl = scriptEl.parentNode;
-
-    insEl.innerHTML = `
-    <iframe src="../index.html"
-            width="300"
-            height="250"
-            style="border:none;overflow:hidden;">
-    </iframe>
-  `;
-})();
+// (function() {
+//     const scriptEl = document.currentScript;
+//     if (!scriptEl) return;
+//     const insEl = scriptEl.parentNode;
+//
+//     insEl.innerHTML = `
+//     <iframe src="../index.html"
+//             width="300"
+//             height="250"
+//             style="border:none;overflow:hidden;">
+//     </iframe>
+//   `;
+// })();
 
 start();
